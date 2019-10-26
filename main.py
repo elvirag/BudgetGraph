085c5c864229525a072e24d3685a4dc0f4d0e5e7
Elvira Gandelman <elvirag@gmail.com>
2019-10-26 17:10:52 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import kivy
+# import KivyCalendar
+import sqlite_stuff
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+class MyGrid(Widget):
+
+	date_purchase = ObjectProperty(None)
+	name = ObjectProperty(None)
+	cost = ObjectProperty(None)
+	category = ObjectProperty(None)
+	pob = ObjectProperty(None)
+	comments = ObjectProperty(None)
+
+	def btn(self):
+		expense = Expense(
+			self.date_purchase.text,
+			self.name.text,
+			self.cost.text,
+			self.category.text,
+			self.pob.text,
+			self.comments.text
+			)
+		sqlite_stuff.create_expense(expense)
+		self.date_purchase.text = ""
+		self.name.text = ""
+		self.cost.text = ""
+		self.category.text = ""
+		self.pob.text = ""
+		self.comments.text = ""
+
+class Expense:
+	def __init__(self, date_purchase, name, cost, category, pob, comments):
+		self.date_purchase = date_purchase
+		self.name = name
+		self.cost = cost
+		self.category = category
+		self.pob = pob
+		self.comments = comments
+	def __str__(self):
+		return " Date of Purchase: {} Name of Expense: {} Amount Spent: {} Category: {} Place of Purchase: {} Comments: {}".format(self.date_purchase, self.name, self.cost, self.category, self.pob, self.comments)
+
+class MyApp(App):
+	def build(self):
+		return MyGrid()
+
+if __name__ == "__main__":
+	MyApp().run()
---
+++ sqlite_stuff.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+def connect():
+
+	conn = sqlite3.connect('expenses_db.sqlite')
+	cur = conn.cursor()
+
+	return conn, cur
+
+def create_table():
+	conn, cur = connect()
+
+	cur.execute('pragma encoding')
+
+	cur.execute("""CREATE TABLE IF NOT EXISTS expenses (
+	    Expense_id INTEGER PRIMARY KEY,
+	    Date_purchase DATE NOT NULL,
+	    Name Varchar(256) NOT NULL,
+	    Cost REAL NOT NULL,
+	    Category Varchar(256) NOT NULL,
+	    Buisness Varchar(256) NOT NULL,
+	    Comments Varchar(1024)
+	);
+	""")
+
+	conn.commit()
+	conn.close()
+
+
+
+def create_expense(expense):
+	conn, cur = connect()
+
+	cur.execute(
+		"""
+		INSERT INTO expenses (Date_purchase, Name, Cost, Category, Buisness, Comments)
+		VALUES( {}, {}, {}, {}, {}, {});
+		""".format(expense.date_purchase, expense.name, expense.cost, expense.category, expense.pob, expense.comments)
+		)
+	conn.commit()
+	conn.close()
+
+create_table()
